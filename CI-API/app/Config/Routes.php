74c1a5abb779ed7f30ece827af2a2fc4349cc7e2
Amir Hakim <amirhakimadilayahya@gmail.com>
2024-06-18 14:21:40 +0700

    Update Routes.php

--- CI-API/app/Config/Routes.php
+++ CI-API/app/Config/Routes.php
@@ -10,4 +10,8 @@ $routes->group('', ['filter' => 'cors'], static function (RouteCollection $route
     $routes->options('product', '\Dummy');
     $routes->options('product/(:any)', '\Dummy');
     $routes->resource('product');
+    $routes->resource('user');
+    // $routes->resource('/login')
 });
+$routes->post('api/login', 'Login::index');
+// $route['/api/login'] = 'Login::login';
