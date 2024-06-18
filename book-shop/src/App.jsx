70b70ade6ebb153433572faf09a3a7d4bf2634cd
Amir Hakim <amirhakimadilayahya@gmail.com>
2024-06-18 14:16:38 +0700

    add routers

--- book-shop/src/App.jsx
+++ book-shop/src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
-import NavBar from './components/navBar';
 import ProductList from './components/productList';
 import AddProduct from './components/addProduct';
 import EditProduct from './components/editProduct';
 import LandingPage from './components/landingPage';
+import Login from './users/login';
+import Product from './components/product';
 
 export default function App() {
   return (
     <>
-      <NavBar />
       <Router>
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<LandingPage />} />
-            <Route path='/product-list' element={<ProductList />} />
-            <Route path='/add' element={<AddProduct />} />
-            <Route path='/edit/:id' element={<EditProduct />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/product-list' element={<ProductList />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/add' element={<AddProduct />} />
+          <Route path='/edit/:id' element={<EditProduct />} />
+        </Routes>
       </Router>
     </>
   )
