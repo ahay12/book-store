92c60722f068c78e2f72a1a7d896cf504ffb8e52
Amir Hakim <amirhakimadilayahya@gmail.com>
2024-06-18 14:18:19 +0700

    Create useDocumentTitle.jsx

---
+++ book-shop/src/components/whyChooseMe.jsx
@@ -0,0 +1,36 @@
+export default function WhyChooseMe() {
+    return (
+        <>
+            <div className="container pb-16">
+                <div className="flex flex-row justify-center">
+                    <div className="w-1/3">
+                        <div className="flex justify-center">
+                            <span className="grid place-content-center bg-primary rounded-md w-[70px] h-[70px] bg-opacity-80"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="size-12">
+                                <path fillRule="evenodd" d="M6.912 3a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H6.912Zm13.823 9.75-2.213-7.191A1.5 1.5 0 0 0 17.088 4.5H6.912a1.5 1.5 0 0 0-1.434 1.059L3.265 12.75H6.11a3 3 0 0 1 2.684 1.658l.256.513a1.5 1.5 0 0 0 1.342.829h3.218a1.5 1.5 0 0 0 1.342-.83l.256-.512a3 3 0 0 1 2.684-1.658h2.844Z" clipRule="evenodd" />
+                            </svg></span>
+                            <h1 className="text-2xl font-bold place-content-center ml-8">Free Shipping</h1>
+                        </div>
+                    </div>
+                    <div className="w-1/3">
+                        <div className="flex justify-center">
+                            <span className="grid place-content-center bg-primary rounded-md w-[70px] h-[70px] bg-opacity-80"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="size-12">
+                                <path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
+                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z" clipRule="evenodd" />
+                            </svg>
+                            </span>
+                            <h1 className="text-2xl font-bold place-content-center ml-8">Best Price</h1>
+                        </div>
+                    </div>
+                    <div className="w-1/3">
+                        <div className="flex justify-center">
+                            <span className="grid place-content-center bg-primary rounded-md w-[70px] h-[70px] bg-opacity-80"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="size-12">
+                                <path fillRule="evenodd" d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z" clipRule="evenodd" />
+                            </svg></span>
+                            <h1 className="text-2xl font-bold place-content-center ml-9">Quick Delivery</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
