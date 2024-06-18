481eb74a3eb80678b0f5b39a5492049993c6e4c3
Amir Hakim <amirhakimadilayahya@gmail.com>
2024-06-18 14:18:46 +0700

    Update tailwind.config.js

--- book-shop/tailwind.config.js
+++ book-shop/tailwind.config.js
@@ -1,15 +1,31 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    fontFamily: {
+      sans: ["Helvetica", "Arial", "sans-serif"],
+    },
+    screens: {
+      "2xl": "1280px",
+    },
     extend: {},
   },
-  // container: {
-  //   center: true,
-  //   padding: "16px",
-  // },
+
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        light: {
+          primary: "#29c6f3",
+          secondary: "#f7d060",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
