d86e59e5e3dfa5519715f4f3c6e380c0c3a278de
Amir Hakim <amirhakimadilayahya@gmail.com>
2024-07-07 19:26:54 +0700

    Update cartContext.jsx

--- book-shop/src/components/cartContext.jsx
+++ book-shop/src/components/cartContext.jsx
@@ -186,19 +186,18 @@ export const CartContext = createContext();
 
 // Create a provider component to wrap your entire application with
 export const CartProvider = ({ children }) => {
-    const initialCart = JSON.parse(sessionStorage.getItem('cart')) || [];
+    const initialCart = JSON.parse(localStorage.getItem('cart')) || [];
 
     const [cart, setCart] = useState(initialCart);
     const [totalBuy, setTotalBuy] = useState(0);
     const [user, setUser] = useState(null);
-    // console.log(user);
+    const [fetchCart, setFetchCart] = useState([]);
 
     useEffect(() => {
         const token = localStorage.getItem('jwtToken'); // Assuming token is stored in localStorage
         if (token) {
             try {
                 const decodedToken = jwtDecode(token);
-                // console.log(decodedToken.id);
                 setUser(decodedToken.sub); // Assuming 'sub' contains the user ID
             } catch (error) {
                 console.error('Failed to decode JWT token', error);
@@ -207,10 +206,14 @@ export const CartProvider = ({ children }) => {
     }, []);
 
     useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cart));
+        // localStorage.setItem('cart', JSON.stringify(cart));
+        localStorage.getItem('cart', JSON.stringify(fetchCart));
         // Calculate totalBuy
-        const total = cart.reduce((acc, item) => acc + item.totalAmount, 0);
+        const total = fetchCart.reduce((acc, item) => acc + parseInt(item.total), 0);
         setTotalBuy(total);
+        // console.log(fetchCart);
+        console.log(totalBuy);
+        // fetchCartFromBackend();
         // Send updated cart to backend
         if (user) {
             updateCartOnBackend(cart, total);
@@ -232,7 +235,9 @@ export const CartProvider = ({ children }) => {
                 }
             });
             const fetchedCart = response.data;
-            setCart(fetchedCart);
+            // console.log('Fetched Cart from backend:', fetchedCart);
+            localStorage.setItem('cart', JSON.stringify(fetchedCart))
+            setFetchCart(fetchedCart);
         } catch (error) {
             console.error('Failed to fetch cart from backend', error);
         }
@@ -253,7 +258,6 @@ export const CartProvider = ({ children }) => {
     };
 
     const addToCart = (product) => {
-
         const foundIndex = cart.findIndex(item => item.id === product.id);
 
         if (foundIndex !== -1) {
@@ -276,19 +280,62 @@ export const CartProvider = ({ children }) => {
         }
     };
 
-    const removeFromCart = async (productId) => {
+    const removeFromCart = async (id) => {
         const token = localStorage.getItem('jwtToken');
         try {
-            await axios.delete(`http://localhost:4000/cart/remove/${productId}`, {
+            await axios.delete(`http://localhost:4000/cart/remove/${id}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json',
                 }
             });
-            const updatedCart = cart.filter(item => item.id !== productId);
+            const updatedCart = cart.filter(item => item.id !== id);
             setCart(updatedCart);
+            fetchCartFromBackend();
         } catch (error) {
             console.error('Failed to remove item from cart on backend', error);
+        }
+    };
+
+    const increaseQuantity = async (id) => {
+        try {
+            const token = localStorage.getItem('jwtToken');
+            const response = await axios.patch(`http://localhost:4000/cart/increaseQuantity/${id}`, {}, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            const updatedCart = cart.map(item => {
+                if (item.id === id) {
+                    return { ...item, quantity: response.data.quantity, totalAmount: response.data.total };
+                }
+                return item;
+            });
+            setCart(updatedCart);
+            fetchCartFromBackend();
+        } catch (error) {
+            console.error('Failed to increase quantity', error);
+        }
+    };
+
+    const decreaseQuantity = async (id) => {
+        try {
+            const token = localStorage.getItem('jwtToken');
+            const response = await axios.patch(`http://localhost:4000/cart/decreaseQuantity/${id}`, {}, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            const updatedCart = cart.map(item => {
+                if (item.id === id) {
+                    return { ...item, quantity: response.data.quantity, totalAmount: response.data.total };
+                }
+                return item;
+            });
+            setCart(updatedCart);
+            fetchCartFromBackend();
+        } catch (error) {
+            console.error('Failed to decrease quantity', error);
         }
     };
 
@@ -299,102 +346,8 @@ export const CartProvider = ({ children }) => {
     const cartItemsCount = cart.reduce((acc, item) => acc + item.quantity, 0);
 
     return (
-        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, cartItemsCount, totalBuy, fetchCartFromBackend }}>
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, increaseQuantity, decreaseQuantity, cartItemsCount, totalBuy, fetchCart, fetchCartFromBackend }}>
             {children}
         </CartContext.Provider>
     );
 };
-
-// import React, { createContext, useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { jwtDecode } from 'jwt-decode';
-
-// // Create a context for managing the cart state
-// export const CartContext = createContext();
-
-// // Create a provider component to wrap your entire application with
-// export const CartProvider = ({ children }) => {
-//     const initialCart = JSON.parse(sessionStorage.getItem('cart')) || [];
-//     const [cart, setCart] = useState(initialCart);
-//     const [totalBuy, setTotalBuy] = useState(0);
-//     const [user, setUser] = useState(null);
-
-//     useEffect(() => {
-//         const token = localStorage.getItem('jwtToken'); // Assuming token is stored in localStorage
-//         if (token) {
-//             try {
-//                 const decodedToken = jwtDecode(token);
-//                 setUser(decodedToken.id); // Assuming 'sub' contains the user ID
-//             } catch (error) {
-//                 console.error('Failed to decode JWT token', error);
-//             }
-//         }
-//     }, []);
-
-//     useEffect(() => {
-//         sessionStorage.setItem('cart', JSON.stringify(cart));
-//         // Calculate totalBuy
-//         const total = cart.reduce((acc, item) => acc + item.totalAmount, 0);
-//         setTotalBuy(total);
-//         // Send updated cart to backend
-//         updateCartOnBackend(cart, total);
-//     }, [cart]);
-
-//     const updateCartOnBackend = async (cart, totalBuy) => {
-//         try {
-//             const token = localStorage.getItem('jwtToken'); // Assuming token is stored in localStorage
-//             await axios.post('http://localhost:4000/cart/add', { cart, totalBuy }, {
-//                 headers: {
-//                     'Authorization': `Bearer ${token}`,
-//                     'Content-Type': 'application/json',
-//                 }
-//             });
-//         } catch (error) {
-//             console.error('Failed to update cart on backend', error);
-//         }
-//     };
-
-//     const addToCart = (product) => {
-//         if (!user) {
-//             console.error('User not authenticated');
-//             return;
-//         }
-
-//         const foundIndex = cart.findIndex(item => item.productId === product.id);
-
-//         if (foundIndex !== -1) {
-//             // Product already exists in cart, increase quantity and update total amount
-//             const updatedCart = [...cart];
-//             updatedCart[foundIndex].quantity += 1;
-//             updatedCart[foundIndex].totalAmount = updatedCart[foundIndex].price * updatedCart[foundIndex].quantity;
-//             setCart(updatedCart);
-//         } else {
-//             // Product doesn't exist in cart, add as new item
-//             const newCartItem = {
-//                 id: product.id,
-//                 nameProduct: product.nameProduct,
-//                 price: product.price,
-//                 quantity: 1,
-//                 totalAmount: product.price, // Initial totalAmount is just the price for 1 item
-//             };
-//             setCart([...cart, newCartItem]);
-//         }
-//     };
-
-//     const removeFromCart = (productId) => {
-//         const updatedCart = cart.filter(item => item.productId !== productId);
-//         setCart(updatedCart);
-//     };
-
-//     const clearCart = () => {
-//         setCart([]);
-//     };
-
-//     const cartItemsCount = cart.reduce((acc, item) => acc + item.quantity, 0);
-
-//     return (
-//         <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, cartItemsCount, totalBuy }}>
-//             {children}
-//         </CartContext.Provider>
-//     );
-// };
